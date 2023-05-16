656eecd6b2e68883f99d8503ffb336b3fda9c5b8
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-05-16 20:40:10 +0900

    音が鳴るコード作成

--- OTO-RPG/Assets/01.Script/PlaySound.cs
+++ OTO-RPG/Assets/01.Script/PlaySound.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class PlaySound : MonoBehaviour
 {
+    public AudioSource audioSource;
+
+
+    public AudioClip C,D,E,F,G,A,B;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            audioSource.PlayOneShot(C);
+            Debug.Log("Cが押されたよ");
+        }
         
     }
 }
+
+   
