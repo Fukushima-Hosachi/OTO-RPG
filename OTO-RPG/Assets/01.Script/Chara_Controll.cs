90a3ffc29592d63e07f52ff3bb6e40cd2ad69f24
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-05-18 00:54:14 +0900

    残りHPとHPバーの追加

--- OTO-RPG/Assets/01.Script/Chara_Controll.cs
+++ OTO-RPG/Assets/01.Script/Chara_Controll.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Chara_Controll : MonoBehaviour
 {
@@ -8,10 +9,17 @@ public class Chara_Controll : MonoBehaviour
     public int MaxDamage;
     public int minDamage;
     public int Damage;
+    public int HP_Now;
+    
+    public Slider slider;	
 
     // Start is called before the first frame update
     void Start()
     {
+        Slider slider = GetComponent<Slider>();
+        slider.minValue = 0;
+        slider.maxValue = HP;
+        slider.value = HP;
         
     }
 
@@ -28,6 +36,8 @@ public class Chara_Controll : MonoBehaviour
 
         HP = HP - Damage;
         Debug.Log( "ダメージは" + Damage + "残りHPは" + HP );
+
+        slider.value = HP;
 
         if ( HP <= 0 )
         {
