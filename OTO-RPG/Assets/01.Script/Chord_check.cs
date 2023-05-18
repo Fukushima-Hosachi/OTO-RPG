9de9f5fc16f532e118a24473cc94835d25685080
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-05-18 23:40:32 +0900

    メモ追加

--- OTO-RPG/Assets/01.Script/Chord_check.cs
+++ OTO-RPG/Assets/01.Script/Chord_check.cs
@@ -1,18 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class Chord_check : MonoBehaviour
 {
+    //1,3,5ならCmajとか2,4,6ならDmとか
+    //Boolで管理する?
+    public int Chord_num;
+    bool CM;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Chord_num = 0;
+        CM = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            Chord_num = 1;
+        }
+
+        Debug.Log( Chord_num );
+
+        //1，3，5，の順番で押されたらCmaj
     }
+
+    void Cmaj()
+    {
+
+    }
+
+    //btnC.onClick.Invoke() → btnE.onClick.Invoke(); →　btnG.onClick.Invoke();の順で押されたら
+   
 }
