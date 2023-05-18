63c6b85847e01b5da4d4bc2e497af9d4da882496
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-05-18 14:29:38 +0900

    Cキー押したらボタンの色も変わる音もなる

--- OTO-RPG/Assets/01.Script/Chord_Gage_Controll.cs
+++ OTO-RPG/Assets/01.Script/Chord_Gage_Controll.cs
@@ -5,27 +5,60 @@ using UnityEngine.UI;
 
 public class Chord_Gage_Controll : MonoBehaviour
 {
-    public Button button;	   
+    public Button button;	
+    
+   
     public Image image; 
+    public AudioClip C;
+    public AudioSource audioSource;
+    
     // Start is called before the first frame update
     void Start()
     {
+        GetComponent<AudioSource>();
+        button = GetComponentInChildren<Button>();
+        image = GetComponentInChildren<Image>();
+      
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Onclick();
+        }
     }
 
     public void Onclick()
-    { 
-       button = GetComponent<Button>();
-       image = GetComponentInChildren<Image>();
-       image.color = new Color( 255, 0, 0 ); 
+    {
+        audioSource.PlayOneShot(C);
 
-       Debug.Log("C");
-       
+        if (button.CompareTag("C")) 
+       {
+
+        button = GetComponent<Button>();
+        image = GetComponentInChildren<Image>();
+        image.color = new Color( 255, 0, 0 ); 
+
+       }
     }
+
+    public void Onclick_C()
+    { 
+       if (button.CompareTag("C")) 
+       {
+
+        button = GetComponent<Button>();
+        image = GetComponentInChildren<Image>();
+        image.color = new Color( 255, 0, 0 ); 
+
+       }
+            
+    }
+
+    
 }
+
+
