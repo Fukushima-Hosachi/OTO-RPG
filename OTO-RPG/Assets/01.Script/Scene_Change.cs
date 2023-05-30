59e17cd85fba32e30d6519a000eacb37c147545f
Fukushima-Hosachi <71868188+Fukushima-Hosachi@users.noreply.github.com>
2023-05-30 19:45:34 +0900

    いろんなアニメーション追加してもうた

--- OTO-RPG/Assets/01.Script/Scene_Change.cs
+++ OTO-RPG/Assets/01.Script/Scene_Change.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class Scene_Change : MonoBehaviour
 {
+    Animator animator;
+    
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.anyKey)
+        {
+            animator.SetBool ("FadeOut", true);
+            Debug.Log("FadeOut");
+            Invoke ( "LoadButtle", 1.5f);
+        }
+    }
+
+    void LoadTitle()
+    {
+        SceneManager.LoadScene("Tittle");
+    }
+
+    
+    void LoadButtle()
+    {
+        SceneManager.LoadScene("Buttle");
         
     }
+
+    
 }
